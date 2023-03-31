01f0a197173cd842588158ca1d863ce5d90c23b8
Ben Buzard <ben@benbuzard.com>
2023-03-31 08:01:51 -0700

    yeah

--- edit.sh
+++ edit.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-#empty
 
+
+file="file path here people!"
 delimiterString=':'
 while IFS="\n" read -r line; do
     IFS=$delimiterString read -a col <<< ${line};
-    DataCol1=${col[0]}
-    DataCol2=${col[2]}
+    DataCol0=${col[0]}
+    DataCol1=${col[1]}
 
+    echo "$DataCol0"
     echo "$DataCol1"
-    echo "$DataCol2"
-done < /etc/passwd
+done < $file
